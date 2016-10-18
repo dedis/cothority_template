92e70c0ebd9dd19a6e4464ced1e92d5805a807b0
Linus Gasser <linus.gasser@epfl.ch>
2016-10-18 16:21:52 +0200

    added local test

--- service/service_test.go
+++ service/service_test.go
@@ -11,15 +11,19 @@ func TestMain(m *testing.M) {
 	log.MainTest(m)
 }
 
+func NewTestClient(lt *sda.LocalTest) *Client {
+	return &Client{Client: lt.NewClient(ServiceName)}
+}
+
 func TestServiceTemplate(t *testing.T) {
-	local := sda.NewTCPTest()
+	local := sda.NewLocalTest()
 	// generate 5 hosts, they don't connect, they process messages, and they
 	// don't register the tree or entitylist
 	_, el, _ := local.GenTree(5, true)
 	defer local.CloseAll()
 
 	// Send a request to the service
-	client := NewClient()
+	client := NewTestClient(local)
 	log.Lvl1("Sending request to service...")
 	duration, err := client.Clock(el)
 	log.ErrFatal(err, "Couldn't send")
