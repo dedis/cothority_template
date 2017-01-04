24c43206331a0dd9502eb44040b25b5cbc52aeed
Linus Gasser <linus.gasser@epfl.ch>
2017-01-04 11:41:57 +0100

    App tesht onet33 (#4)
    * following onet/app

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+DBG_TEST=1
+# Debug-level for app
+DBG_APP=2
+
+. $GOPATH/src/github.com/dedis/onet/app/libtest.sh
+
+main(){
+    startTest
+    buildCothority
+	test Build
+	test Main
+    stopTest
+}
+
+testMain(){
+	testGrep Main runTmpl main
+}
+
+testBuild(){
+    testOK dbgRun runTmpl --help
+}
+
+runTmpl(){
+    dbgRun ./$APP -d $DBG_APP $@
+}
+
+main
