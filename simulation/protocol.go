c28fd94cb7b6929b58d29f34c75546c81ddb05b3
Linus Gasser <linus.gasser@epfl.ch>
2017-01-13 08:56:52 +0100

    adjusting structures

--- simulation/protocol.go
+++ simulation/protocol.go
@@ -67,7 +67,7 @@ func (s *SimulationProtocol) Setup(dir string, hosts []string) (
 // SimulationBFTree structure which will load the roster- and the
 // tree-structure to speed up the first round.
 func (s *SimulationProtocol) Node(config *onet.SimulationConfig) error {
-	index, _ := config.Roster.Search(config.Conode.ServerIdentity.ID)
+	index, _ := config.Roster.Search(config.Server.ServerIdentity.ID)
 	if index < 0 {
 		log.Fatal("Didn't find this node in roster")
 	}
