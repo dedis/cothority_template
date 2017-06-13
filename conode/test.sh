05471a04c2bf6d83ac29094d6fa814c7b4a5ec9c
Linus Gasser <linus.gasser@epfl.ch>
2017-06-13 10:31:07 +0200

    simplyfying the conode.go

--- conode/test.sh
+++ conode/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DBG_TEST=1
+DBG_APP=2
 
 . $GOPATH/src/gopkg.in/dedis/onet.v1/app/libtest.sh
 
@@ -8,17 +9,7 @@ main(){
     startTest
 	setupConode
     test Build
-    test Conode
     stopTest
-}
-
-testConode(){
-    runCoBG 1 2
-    cp co1/public.toml .
-    tail -n 4 co2/public.toml >> public.toml
-    testOK runCo 1 check -g public.toml
-    tail -n 4 co3/public.toml >> public.toml
-    testFail runCo 1 check -g public.toml
 }
 
 testBuild(){
