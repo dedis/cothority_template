3494943fa3176a23a397170d4b7195a2a71128bf
Linus Gasser <linus.gasser@epfl.ch>
2017-08-18 12:57:38 +0200

    adding websocket-class

--- html/js/bundle.js
+++ html/js/bundle.js
@@ -161,54 +161,14 @@ var CothorityMessages = function (_CothorityProtobuf) {
     }
 
     createClass(CothorityMessages, [{
-        key: 'buf2hex',
+        key: 'createClockRequest',
 
-
-        /**
-         * Converts an arraybuffer to a hex-string
-         * @param {ArrayBuffer} buffer
-         * @returns {string} hexified ArrayBuffer
-         */
-        value: function buf2hex(buffer) {
-            // buffer is an ArrayBuffer
-            return Array.prototype.map.call(new Uint8Array(buffer), function (x) {
-                return ('00' + x.toString(16)).slice(-2);
-            }).join('');
-        }
-
-        /**
-         * Converts a toml-string of public.toml to a roster that can be sent
-         * to a service. Also calculates the Id of the ServerIdentities.
-         * @param {string} toml of public.toml
-         * @returns {object} Roster-object
-         */
-
-    }, {
-        key: 'toml_to_roster',
-        value: function toml_to_roster(toml) {
-            var parsed = {};
-            var b2h = this.buf2hex;
-            try {
-                parsed = topl.parse(toml);
-                parsed.servers.forEach(function (el) {
-                    var pubstr = Uint8Array.from(atob(el.Public), function (c) {
-                        return c.charCodeAt(0);
-                    });
-                    var url = "https://dedis.epfl.ch/id/" + b2h(pubstr);
-                    el.Id = new UUID(5, "ns:URL", url).export();
-                });
-            } catch (err) {}
-            return parsed;
-        }
 
         /**
          * Create an encoded message to make a ClockRequest to a cothority node
          * @param {Array} servers - list of ServerIdentity
          * @returns {*|Buffer|Uint8Array}
          */
-
-    }, {
-        key: 'createClockRequest',
         value: function createClockRequest(servers) {
             var fields = {
                 Roster: {
@@ -255,6 +215,105 @@ var CothorityMessages = function (_CothorityProtobuf) {
             response = new Uint8Array(response);
 
             return this.decodeMessage('CountResponse', response);
+        }
+        /**
+         * Use the existing socket or create a new one if required
+         * @param socket - WebSocket-array
+         * @param address - String ws address
+         * @param message - ArrayBuffer the message to send
+         * @param callback - Function callback when a message is received
+         * @param error - Function callback if an error occurred
+         * @returns {*}
+         */
+
+    }, {
+        key: 'createSocket',
+        value: function createSocket(socket, address, message, callback, error) {
+            if (!socket) {
+                socket = {};
+            }
+            var sock = socket[address];
+            if (!sock || sock.readyState > 2) {
+                sock = new WebSocket(address);
+                sock.binaryType = 'arraybuffer';
+                socket[address] = sock;
+            }
+
+            function onError(e) {
+                sock.removeEventListener('error', onError);
+                error(e);
+            }
+            sock.addEventListener('error', onError);
+
+            function onMessage(e) {
+                sock.removeEventListener('message', onMessage);
+                callback(e.data);
+            }
+            sock.addEventListener('message', onMessage);
+
+            if (sock.readyState === 0) {
+                sock.addEventListener('open', function () {
+                    sock.send(message);
+                });
+            } else {
+                sock.send(message);
+            }
+
+            return socket;
+        }
+
+        /**
+         * Converts an arraybuffer to a hex-string
+         * @param {ArrayBuffer} buffer
+         * @returns {string} hexified ArrayBuffer
+         */
+
+    }, {
+        key: 'buf2hex',
+        value: function buf2hex(buffer) {
+            // buffer is an ArrayBuffer
+            return Array.prototype.map.call(new Uint8Array(buffer), function (x) {
+                return ('00' + x.toString(16)).slice(-2);
+            }).join('');
+        }
+
+        /**
+         * Converts a toml-string of public.toml to a roster that can be sent
+         * to a service. Also calculates the Id of the ServerIdentities.
+         * @param {string} toml of public.toml
+         * @returns {object} Roster-object
+         */
+
+    }, {
+        key: 'toml_to_roster',
+        value: function toml_to_roster(toml) {
+            var parsed = {};
+            var b2h = this.buf2hex;
+            try {
+                parsed = topl.parse(toml);
+                parsed.servers.forEach(function (el) {
+                    var pubstr = Uint8Array.from(atob(el.Public), function (c) {
+                        return c.charCodeAt(0);
+                    });
+                    var url = "https://dedis.epfl.ch/id/" + b2h(pubstr);
+                    el.Id = new UUID(5, "ns:URL", url).export();
+                });
+            } catch (err) {}
+            return parsed;
+        }
+
+        /**
+         * Returns a websocket-url from a ServerIdentity
+         * @param {ServerIdentity} the serveridentity to convert to a websocket-url
+         * @returns {string} the url
+         */
+
+    }, {
+        key: 'si_to_ws',
+        value: function si_to_ws(si, path) {
+            var ip_port = si.Address.replace("tcp://", "").split(":");
+            ip_port[1] = parseInt(ip_port[1]) + 1;
+            return "ws://" + ip_port.join(":") + path;
         }
     }]);
     return CothorityMessages;
