e105272a40ee6cea25fdb65308880920f1a85f8b
Noémien Kocher <nkcr.je@gmail.com>
2019-10-22 14:15:45 +0200

    Adds the specs

---
+++ external/js/spec/index.spec.ts
@@ -0,0 +1,15 @@
+import Docker from "dockerode";
+import { byzcoin } from "@dedis/cothority";
+
+describe("Module import Tests", () => {
+    it("should import the module", () => {
+        expect(byzcoin.ByzCoinRPC).toBeDefined();
+    });
+});
+
+describe("Docker should be available", () => {
+    it("should not yield an error when getting docker infos", async () => {
+        const docker = new Docker();
+        await expectAsync(docker.info()).toBeResolved();
+    });
+});
